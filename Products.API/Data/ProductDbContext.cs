20092050223833bd7e584e088aff659bd0e87983
Hasan H. SAHIN <hsnsh@outlook.com>
2022-11-03 15:31:59 +0300

    first commit

---
+++ Products.API/Data/ProductDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Products.API.Models;
+
+namespace Products.API.Data
+{
+    public class ProductDbContext : DbContext
+    {
+        public ProductDbContext(DbContextOptions<ProductDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
